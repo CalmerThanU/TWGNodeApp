71c2bfdfb4360296df80f337ec539ea0d70e28d0
CalmerThanU <timgreenwalton@hotmail.com>
2018-03-15 21:14:25 -0400

    Update index.js
    plural

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello AKA consultant World!");
+    response.end("Hello AKA consultants World!");
 
 });
 
